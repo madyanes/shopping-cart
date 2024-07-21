d67e5009a0cb5222e4a5c8dc5ca3f93c0da52f2d
madyanes <madyanes.dev@gmail.com>
2024-07-21 10:42:57 +0700

    Display product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  return <ShoppingCart />
+function getAllProducts() {
+  return fetch('https://fakestoreapi.com/products?limit=10').then((res) =>
+    res.json()
+  )
 }
 
-function ProductList() {}
+function App() {
+  return (
+    <>
+      <ShoppingCart />
+      <ProductList />
+    </>
+  )
+}
 
-function Product() {}
+function ProductList() {
+  const [allProducts, setAllProducts] = useState([])
+
+  useEffect(() => {
+    getAllProducts().then((products) => setAllProducts(products))
+  }, [])
+
+  return (
+    <ul>
+      {allProducts.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+    </ul>
+  )
+}
+
+function Product({ product }) {
+  return (
+    <li>
+      <div className='product-image'>
+        <img src={product.image} alt='' />
+      </div>
+      <div className='product-text'>
+        <div>
+          <div className='product-title'>{product.title}</div>
+          <div className='product-price'>{product.price}</div>
+        </div>
+        <div>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </li>
+  )
+}
 
 function ShoppingCart() {
   return (
