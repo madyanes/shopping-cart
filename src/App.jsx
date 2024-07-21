e81fd0fe25be1c0132e5e1590e2d1ed310c2493a
madyanes <madyanes.dev@gmail.com>
2024-07-21 09:49:32 +0700

    Create static shopping cart form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import './App.css'
 
 function App() {
-  return <h1>Shopping Cart</h1>
+  return <ShoppingCart />
+}
+
+function ProductList() {}
+
+function Product() {}
+
+function ShoppingCart() {
+  return (
+    <form>
+      <div>
+        <label htmlFor=''>Total Items</label>
+        <input type='text' value='0' disabled />
+      </div>
+
+      <div>
+        <label htmlFor=''>Total price</label>
+        <input type='text' value='0' disabled />
+      </div>
+    </form>
+  )
 }
 
 export default App
